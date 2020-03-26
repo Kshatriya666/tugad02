f7c95389fee0824a6cfc0f733112eda0368a40bb
Kshatriya666 <62422177+Kshatriya666@users.noreply.github.com>
2020-03-26 20:19:33 +0800

    Make Input String into Variable

--- InputOutput.java
+++ InputOutput.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
+  String name;
+  System.out.print("Insert your name : ");
+  name = scanner.nextLine();
 
  }
 }
