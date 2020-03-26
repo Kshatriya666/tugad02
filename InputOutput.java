9daa725d2c3bfe1ce5ddc09033f5c7cc41e8780c
Kshatriya666 <62422177+Kshatriya666@users.noreply.github.com>
2020-03-26 20:44:08 +0800

    Finishing + Modified

--- InputOutput.java
+++ InputOutput.java
@@ -4,8 +4,20 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String name;
+  int SKS;
+  double IPK;
+
   System.out.print("Insert your name : ");
   name = scanner.nextLine();
   System.out.println("Hi, "+name);
+
+  System.out.print("Insert how many SKS you already pass on : ");
+  SKS = scanner.nextInt();
+
+  System.out.print("Insert your latest IPK score : ");
+  IPK = scanner.nextDouble();
+
+  double result = IPK * SKS;
+  System.out.print("Total Score = "+result);
  }
 }
